59cc140b25c8e086522f3e2d2df73caee7931ad0
Sabeenirfan <133214650+Sabeenirfan@users.noreply.github.com>
2025-03-19 11:11:46 +0500

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World so  ww !");
+        System.out.println("Hello, World so  ww nn !");
     }
 }
